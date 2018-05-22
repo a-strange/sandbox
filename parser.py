39f420abcb2f8b53de74a5ffe1d51039b7b25396
cytoramanela <35495502+cytoramanela@users.noreply.github.com>
2018-05-22 15:16:22 +0100

    ENH: Add building number parser to Cyutils (#92)
        Bump version to 1.0.4

--- parser.py
+++ parser.py
@@ -1,4 +1,5 @@
 import re
+import warnings
 
 from cyutils.address.road_types import STREET_NAMES
 from cyutils.address.road_types import STREET_ABBREVIATION_TO_NAME
@@ -8,8 +9,19 @@ street_desc = "ST\.?|SAINT|KING|QUEEN|UPPER|LOWER|HALF|GREAT|WEST|EAST|NORTH|SOU
 street_names = '|'.join(STREET_NAMES)
 street_loc = "NORTH[,]|SOUTH[,]|EAST[,]|WEST[,]"
 ROAD_PATTERN = r"((\b({}) )?[A-Za-z']+ ({})\b( {})?)"
-ROAD_PATTERN = ROAD_PATTERN.format(street_desc, street_names, street_loc)
-POSTCODE_PATTERN = r'(\b[A-Z]{1,2}[0-9][0-9A-Z]? ?([0-9][A-Z]{2}))'
+ROAD_PATTERN = re.compile(ROAD_PATTERN.format(street_desc,
+                                              street_names,
+                                              street_loc))
+
+POSTCODE_PATTERN = re.compile(r'(\b[A-Z]{1,2}[0-9][0-9A-Z]? ?([0-9][A-Z]{2}))')
+
+number_prefix = 'UNIT|UNITS|FLAT|FLATS'
+numeric_part_1 = '\d*-?\d+[A-Z/]*'
+numeric_part_2 = '(-\d*[A-Z]*)?'
+NUMBER_PATTERN = r"((\b({}) )?({})+({}))"
+NUMBER_PATTERN = re.compile(NUMBER_PATTERN.format(number_prefix,
+                                                  numeric_part_1,
+                                                  numeric_part_2))
 
 
 def process_capture_groups(group):
@@ -24,7 +36,7 @@ def process_capture_groups(group):
 
 def street_address(full_address):
     full_address = normalise_address(full_address)
-    capture_groups = re.findall(ROAD_PATTERN, full_address)
+    capture_groups = ROAD_PATTERN.findall(full_address)
     if capture_groups:
         name, _type = process_capture_groups(capture_groups)
         return '{} {}'.format(name, _type)
@@ -32,8 +44,30 @@ def street_address(full_address):
 
 
 def postcode(full_address):
+    return capture_address_element(POSTCODE_PATTERN, full_address)
+
+
+def number(full_address):
+    warning_message = (
+        """ \n This parser should be used with the knowledge that this
+            function is open to four significant vulnerabilities:
+               1) `number()` will parse the first numeric characters it
+                  an address string contains (read from left to right).
+                  If the address string has:
+                    a) no building number
+                    b) numeric characters unrelated to addressable
+                       information at the start of the address string
+               2) Address numbers separated by `&` or `,` will not be parsed
+               3) Building names that include numeric characters are
+                  incorrectly parsed as building numbers\n """
+    )
+    warnings.warn(warning_message)
+    return capture_address_element(NUMBER_PATTERN, full_address)
+
+
+def capture_address_element(regex_object, full_address):
     full_address = normalise_address(full_address)
-    capture_groups = re.findall(POSTCODE_PATTERN, full_address)
+    capture_groups = regex_object.findall(full_address)
     if capture_groups:
         return capture_groups[0][0]
     return ''
