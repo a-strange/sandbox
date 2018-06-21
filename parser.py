db4d32ef190fd0866a370c752a02679478188e0e
A Strange <34027026+a-strange@users.noreply.github.com>
2018-06-21 16:46:27 +0100

    DOC: Added address.parser (#104)
    * Add docs and refactor capture address element
    Add documentation for overall module and each function, and
    refactor to use search rather than findall to select the first
    element.
    
    * Add rst files
    Add docs to allow building to HTML, tweak formatting and type
    annotations for display purposes.
    
    * Add doctests, to be extra
    Add doctests to number, postcode and street address, the other
    functions are then tested implicitly.
    
    This resolves: #98

--- parser.py
+++ parser.py
@@ -1,3 +1,17 @@
+"""
+Parser module for common address type transformations, i.e. normalising an
+address string, extracting building number or expanding street type
+abbreviations.
+
+Example
+-------
+    >>> var = parser.street_address('privet dr')
+    >>> var
+    PRIVET DRIVE
+
+Functions
+---------
+"""
 import re
 import warnings
 
@@ -25,6 +39,15 @@ NUMBER_PATTERN = re.compile(NUMBER_PATTERN.format(number_prefix,
 
 
 def process_capture_groups(group):
+    """
+    For a list of matched ROAD_PATTERN objects, remove any commas, split on
+    whitespace and if the last token is an abbreviation of a road type, replace
+    it with the full string.
+
+    :param list[tuple[str]] group: matched ROAD_PATTERN capture groups
+    :returns: (concatenated address, extended abbreviation)
+    :rtype: tuple[str, str]
+    """
     group = group[0][0].replace(',', '')
     group = group.split(' ')
     street_type = group[-1]
@@ -35,6 +58,18 @@ def process_capture_groups(group):
 
 
 def street_address(full_address):
+    """
+    Parse a street address string, normalising the string and replacing
+    common abbreviations with expanded strings.
+
+    .. doctest::
+        >>> street_address('privet dr')
+        PRIVET DRIVE
+
+    :param str full_address: street address to parse
+    :returns: parsed street address
+    :rtype: str
+    """
     full_address = normalise_address(full_address)
     capture_groups = ROAD_PATTERN.findall(full_address)
     if capture_groups:
@@ -44,34 +79,74 @@ def street_address(full_address):
 
 
 def postcode(full_address):
+    """
+    Parse a UK style postcode from a string.
+
+    .. doctest::
+        >>> postcode('9 Bywater St Chelsea, London SW3 4XD')
+        SW3 4XD
+
+    :param str full_address: string to parse
+    :returns: matched postcode
+    :rtype: str
+    """
     return capture_address_element(POSTCODE_PATTERN, full_address)
 
 
 def number(full_address):
-    warning_message = (
-        """ \n This parser should be used with the knowledge that this
-            function is open to four significant vulnerabilities:
-               1) `number()` will parse the first numeric characters it
-                  an address string contains (read from left to right).
-                  If the address string has:
-                    a) no building number
-                    b) numeric characters unrelated to addressable
-                       information at the start of the address string
-               2) Address numbers separated by `&` or `,` will not be parsed
-               3) Building names that include numeric characters are
-                  incorrectly parsed as building numbers\n """
-    )
+    """
+    Parse a building number from a string. Prints a warning related to use.
+
+    .. doctest::
+        >>> number('221 Baker Street')
+        221
+
+    :param str full_address: string to search for a building number
+    :returns: matched building number
+    :rtype: str
+    """
+    warning_message = """\n
+        This parser should be used with the knowledge that this
+        function is open to four significant vulnerabilities:
+           1) `number()` will parse the first numeric characters it
+              an address string contains (read from left to right).
+              If the address string has:
+                a) no building number
+                b) numeric characters unrelated to addressable
+                   information at the start of the address string
+           2) Address numbers separated by `&` or `,` will not be parsed
+           3) Building names that include numeric characters are
+              incorrectly parsed as building numbers\n
+    """
     warnings.warn(warning_message)
     return capture_address_element(NUMBER_PATTERN, full_address)
 
 
 def capture_address_element(regex_object, full_address):
+    """
+    Search a string for the first instance of a regex pattern, returning the
+    matched string. Return empty string if no match.
+
+    :param _sre.SRE_Pattern regex_object: compiled regex pattern
+    :param str full_address: string to search
+    :returns: matched element or empty string
+    :rtype: str
+    """
     full_address = normalise_address(full_address)
-    capture_groups = regex_object.findall(full_address)
+    capture_groups = regex_object.search(full_address)
     if capture_groups:
-        return capture_groups[0][0]
+        return capture_groups[0]
     return ''
 
 
 def normalise_address(address):
+    """
+    Perform a normalisation of a string, replacing any whitespace characters
+    with a single space, removing spaces prior to commas and uppercasing
+    the string.
+
+    :param str address: string to be normalised
+    :returns: normalised string
+    :rtype: str
+    """
     return re.sub('\s+', ' ', str(address).upper()).replace(' ,', ',')
